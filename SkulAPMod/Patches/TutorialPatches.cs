5092356a68616d8457abbac81fb1874eb2d4c304
Jeff Turpin <robotzurg@gmail.com>
2026-04-05 02:41:22 -0600

    Force tutorial to be off

---
+++ SkulAPMod/Patches/TutorialPatches.cs
@@ -0,0 +1,16 @@
+﻿using HarmonyLib;
+
+namespace SkulAPMod.Patches
+{
+    [HarmonyPatch(typeof(Data.GameData.Generic.Tutorial), "isPlayed")]
+    public static class Tutorial_isPlayed_Patch
+    {
+        static void Postfix(ref bool __result)
+        {
+            if (SkulAPMod.APClient.IsConnected)
+            {
+                __result = true;
+            }
+        }
+    }
+}
